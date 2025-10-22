24ea62f35669cefa1cd9798da504ec8f4ecfa0c9
neeharikakoniki <neeharika.koniki@gmail.com>
2025-10-22 15:37:18 -0400

    Added AppHeader

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AppSaveView from '../../components/views/AppSaveView'
+import HomeHeader from '../../components/headers/HomeHeader'
 
 const HomeScreen = () => {
   return (
-    <View>
+    <AppSaveView>
+      <HomeHeader/>
       <Text>HomeScreen</Text>
-    </View>
+    </AppSaveView>
   )
 }
 
